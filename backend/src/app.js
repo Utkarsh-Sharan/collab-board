0ffb5d88103b4d730f0964060c2dc1d8a53e96ba
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-01-01 12:53:04 +0530

    setup cors and api response and error

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+//CORS config
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+//basic config
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 app.get("/test", (req, res) => {
   res.send("Welcome to test!");
