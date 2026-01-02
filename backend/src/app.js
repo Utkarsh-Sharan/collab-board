6e55a6b798ef12f4ba03f1e171d2a12f8294cb2e
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-01-02 15:54:48 +0530

    create login user controller and route

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 //Routes
 import authRouter from "./routes/auth.route.js";
