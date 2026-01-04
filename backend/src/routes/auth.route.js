5a5cea94fd1d90beda84bbe8bb780da0d5f687bb
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-01-04 08:27:37 +0530

    create get current user controller and route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getCurrentUser,
+} from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -9,6 +14,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 
 //Secured routes
+router.route("/current-user").post(verifyJWT, getCurrentUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
