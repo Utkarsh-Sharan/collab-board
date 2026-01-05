97ba0277e1cd1a0a32b531ebc61695c8404a7e1e
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-01-05 12:16:15 +0530

    create validator middleware and update auth routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -8,14 +8,20 @@ import {
   resetForgotPassword,
 } from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { userForgotPasswordValidator, userLoginValidator, userRegisterValidator, userResetForgotPasswordValidator } from "../validators/index.js";
+import { validate } from "../middlewares/validator.middleware.js";
 
 const router = Router();
 
 //Un-secured routes
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
-router.route("/forgot-password").post(forgotPasswordRequest);
-router.route("/reset-password/:resetToken").post(resetForgotPassword);
+router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, loginUser);
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
 
 //Secured routes
 router.route("/current-user").post(verifyJWT, getCurrentUser);
