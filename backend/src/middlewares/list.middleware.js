cf291a8042b3e94b6f03e1e7036de7d52969dc4e
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-01-15 12:40:05 +0530

    create list middleware

---
+++ backend/src/middlewares/list.middleware.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { List } from "../models/list.model.js";
+import { ApiError } from "../utils/Api-Error";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyList = asyncHandler(async (req, res, next) => {
+  const board = req.board;
+  const listId = req.params.listId;
+
+  if(!mongoose.Types.ObjectId.isValid(listId))
+    throw new ApiError(400, "Invalid list ID!");
+
+  const list = await List.findOne({
+    _id: listId,
+    boardId: board._id,
+  });
+
+  if(!list) throw new ApiError(404, "List not found!");
+
+  req.list = list;
+  next();
+});
