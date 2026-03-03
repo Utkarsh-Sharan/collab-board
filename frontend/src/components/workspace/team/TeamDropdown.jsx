6377e1a06fbc8835428e4cddd39c885036bca5fa
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-03-03 12:35:45 +0530

    polish team dropdown and create new board form component

--- frontend/src/components/workspace/team/TeamDropdown.jsx
+++ frontend/src/components/workspace/team/TeamDropdown.jsx
@@ -3,13 +3,15 @@ import TeamMember from "./TeamMember";
 function TeamDropdown({ members }) {
   return (
     <>
-      <article className="absolute left-0 top-10 w-60 border border-gray-200 light-background rounded-lg text-left p-2">
+      <article className="absolute z-10 shadow-lg left-0 top-10 w-60 border border-gray-200 light-background rounded-lg text-left p-2">
         <h3 className="text-xl font-semibold mb-3">Board Members</h3>
 
-        {members.length &&
-          members.map((member) => (
-            <TeamMember key={member.userId} member={member} />
-          ))}
+        <div className="max-h-28 overflow-y-auto space-y-2">
+          {members.length &&
+            members.map((member) => (
+              <TeamMember key={member.userId} member={member} />
+            ))}
+        </div>
 
         <button className="w-full bg-orange-400 rounded-md mt-3 py-1">
           Invite a user
--- frontend/src/modals/CreateNewBoardModal.jsx
+++ frontend/src/modals/CreateNewBoardModal.jsx
@@ -1,35 +1,12 @@
-import { useState } from "react";
 import { useWorkspaceStore } from "../store/useWorkspace.store";
-import { LoaderIcon, X } from "lucide-react";
+import { X } from "lucide-react";
+import CreateNewBoardForm from "../components/forms/CreateNewBoardForm";
 
 function CreateNewBoardModal() {
-  const [newBoardFormData, setNewBoardFormData] = useState({
-    title: "",
-    description: "",
-  });
-  const {
-    isLoading,
-    isCreatingNewBoard,
-    toggleNewBoardCreationModal,
-    createBoard,
-  } = useWorkspaceStore();
+  const { isCreatingNewBoard, toggleNewBoardCreationModal } =
+    useWorkspaceStore();
 
   if (!isCreatingNewBoard) return null;
-
-  const handleChange = (e) => {
-    setNewBoardFormData((prev) => ({
-      ...prev,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = () => {
-    createBoard(newBoardFormData);
-    setNewBoardFormData({
-      title: "",
-      description: "",
-    })
-  };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
@@ -52,41 +29,7 @@ function CreateNewBoardModal() {
           </p>
         </div>
 
-        <form>
-          <div className="mt-10">
-            <h4 className="text-sm font-light">Title</h4>
-            <input
-              type="text"
-              placeholder="Give a title to your board"
-              className="w-full px-5 py-2 border border-orange-100 rounded-md"
-              name="title"
-              value={newBoardFormData.title}
-              onChange={handleChange}
-            />
-
-            <h4 className="mt-5 text-sm font-light">Description</h4>
-            <input
-              type="text"
-              placeholder="Add a description"
-              className="w-full px-5 py-2 border border-orange-100 rounded-md"
-              name="description"
-              value={newBoardFormData.description}
-              onChange={handleChange}
-            />
-
-            <button
-              className="mt-10 bg-orange-400 w-full rounded-md py-3 text-lg font-semibold flex justify-center"
-              disabled={isLoading}
-              onClick={handleSubmit}
-            >
-              {isLoading ? (
-                <LoaderIcon className="w-full h-5 animate-spin" />
-              ) : (
-                "Create new board"
-              )}
-            </button>
-          </div>
-        </form>
+        <CreateNewBoardForm />
       </div>
     </div>
   );
