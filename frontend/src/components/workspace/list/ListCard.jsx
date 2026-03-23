0cd86e06c599f327bf4f9c2eb009b3a00897cf71
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-03-23 12:49:36 +0530

    polish List Card

--- frontend/src/components/workspace/list/ListCard.jsx
+++ frontend/src/components/workspace/list/ListCard.jsx
@@ -1,21 +1,39 @@
-import { Plus } from "lucide-react";
+import { PenBox, Plus, Trash2 } from "lucide-react";
 
 function ListCard() {
   return (
     <article className="flex flex-col gap-5 bg-teal-200 rounded-md px-2 py-4">
-      <div className="flex justify-start items-center gap-4">
-        <p className="font-medium">&lt;list-title&gt;</p>
+      <div className="flex justify-between items-center">
+        <div className="flex justify-start items-center gap-2">
+          <p className="font-medium">&lt;list-title&gt;</p>
+          <p className="text-teal-500 bg-white/60 rounded-full text-center px-2">
+            1
+          </p>
+        </div>
 
-        <p className="rounded-full bg-teal-100 text-teal-500 text-center px-2">
-          1
-        </p>
+        <div className="flex justify-center items-center gap-2">
+          <button>
+            <PenBox
+              className="text-teal-500 bg-white/60 p-1 rounded-md"
+              size={25}
+            />
+          </button>
+          <button>
+            <Trash2
+              className="text-red-300 bg-white/60 p-1 rounded-md"
+              size={25}
+            />
+          </button>
+        </div>
       </div>
 
-      <div className="w-full flex justify-start items-center gap-1 text-teal-700">
+      {/* Add tasks here */}
+
+      <button className="w-1/2 flex justify-start items-center gap-1 text-teal-700 cursor-pointer">
         <Plus size={20} />
 
         <p className="text-base">Add a task</p>
-      </div>
+      </button>
     </article>
   );
 }
